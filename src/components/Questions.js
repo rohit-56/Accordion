9f43511862a4a62986ec003728153566fe5ee803
Rohit <rohit.sharma2@hexaviewtech.com>
2022-05-26 12:09:34 +0530

    export questions of array

---
+++ src/components/Questions.js
@@ -0,0 +1,22 @@
+export const questions = [
+  {
+    id: 1,
+    question: "What is react ?",
+    answer: "React is javascript framwork for frontend.",
+  },
+  {
+    id: 2,
+    question: "What is Angular ?",
+    answer: "angular is javascript framwork for frontend.",
+  },
+  {
+    id: 3,
+    question: "What is spring boot ?",
+    answer: "Spring Boot is java framwork for frontend.",
+  },
+  {
+    id: 4,
+    question: "What is spring ?",
+    answer: "Spring is java framwork for frontend.",
+  },
+];
